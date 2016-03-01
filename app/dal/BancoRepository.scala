e270f7914876fa14ce71ee284628908aa0dec199
llll <luis.arce22@gmail.com>
2016-03-01 10:30:55 -0400

    Merge

--- app/dal/BancoRepository.scala
+++ app/dal/BancoRepository.scala
@@ -67,6 +67,7 @@ class BancoRepository @Inject() (dbConfigProvider: DatabaseConfigProvider)(impli
   def getCuentaById_1(id: Long): Future[Seq[Banco]] = db.run {
     bancos.filter(_.id === id).result
   }
+  
   def listNames(): Future[Seq[String]] = db.run {
     bancos.map(_.nombre).result
   }
