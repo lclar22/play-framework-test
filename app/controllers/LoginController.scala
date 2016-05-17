b7a954e806cf7680737294ec2880280d1a243efe
Luis Arce <luis.arce@jalasoft.com>
2016-05-17 08:45:26 -0400

    Removed source code not used

--- app/controllers/LoginController.scala
+++ app/controllers/LoginController.scala
@@ -61,23 +61,6 @@ class LoginController @Inject() (repo: UserRepository, val messagesApi: Messages
             Ok(views.html.login(newForm))
           }
         }, 3000.millis)
-        /*repo.getByLogin(res.user, res.password).map { res2 =>
-          if (res2.length > 0) {
-            Ok("Welcome!").withSession("userSecurity" -> res2(0).nombre, "userSecurity2" -> res2(0).nombre)
-            if (res2(0).type_1 == "Admin") {
-              Ok(views.html.index(new MyDeadboltHandler))
-            } else if (res2(0).type_1 == "Veterinario") {
-              Ok(views.html.veterinario_profile2(res2(0)))
-            } else if (res2(0).type_1 == "Insumo") {
-              Ok(views.html.insumo_profile2(res2(0)))
-            } else if (res2(0).type_1 == "Almacen") {
-              Ok(views.html.storekeeper_profile2(res2(0)))
-            }
-          } else {
-            println("NO logged");
-            Ok(views.html.login(newForm))
-          }
-        }*/
       }
     )
   }
