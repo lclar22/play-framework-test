dc6aa208598ddcfe57f7deee0fc4ef0e43e2d7ec
llll <luis.arce22@gmail.com>
2016-05-24 04:06:31 -0400

    Updated insumo request to add a product

---
+++ app/controllers/RequestRowByInsumoController.scala
@@ -0,0 +1,192 @@
+package controllers
+
+import scala.concurrent.duration._
+import play.api._
+import play.api.mvc._
+import play.api.i18n._
+import play.api.data.Form
+import play.api.data.Forms._
+import play.api.data.validation.Constraints._
+import play.api.libs.json.Json
+import models._
+import dal._
+import scala.concurrent.{ ExecutionContext, Future, Await }
+import scala.collection.mutable.ListBuffer
+import java.util.LinkedHashMap
+import collection.mutable
+import scala.collection.mutable.ArrayBuffer
+import play.api.data.format.Formats._ 
+
+import javax.inject._
+
+class RequestRowByInsumoController @Inject() (repo: RequestRowRepository, repoProductReq: ProductRequestRepository,
+                                      repoInsum: ProductRepository, repoProductor: ProductorRepository, val messagesApi: MessagesApi)
+                                      (implicit ec: ExecutionContext) extends Controller with I18nSupport {
+
+  val newForm: Form[CreateRequestRowByInsumoForm] = Form {
+    mapping(
+      "requestId" -> longNumber,
+      "productId" -> longNumber,
+      "quantity" -> number,
+      "precio" -> of[Double],
+      "status" -> text
+    )(CreateRequestRowByInsumoForm.apply)(CreateRequestRowByInsumoForm.unapply)
+  }
+
+  val unidades = scala.collection.immutable.Map[String, String]("1" -> "Unidad 1", "2" -> "Unidad 2")
+
+  def index = Action {
+    val productReqNames = getProductReqNamesMap()
+    val insumoNames = getInsumoNamesMap()
+    Ok(views.html.requestRowByInsumo_index(productReqNames, insumoNames))
+  }
+
+  def addGet = Action {
+    val productReqNames = getProductReqNamesMap()
+    val insumoNames = getInsumoNamesMap()
+    Ok(views.html.requestRowByInsumo_add(newForm, productReqNames, insumoNames))
+  }
+
+  def add = Action.async { implicit request =>
+    newForm.bindFromRequest.fold(
+      errorForm => {
+        Future.successful(Ok(views.html.requestRowByInsumo_index(Map[String, String](), Map[String, String]())))
+      },
+      res => {
+        repo.create(res.requestId, res.productId, res.quantity, res.precio, res.status).map { _ =>
+          Redirect(routes.ProductRequestByInsumoController.show(res.requestId))
+        }
+      }
+    )
+  }
+
+  def getRequestRowByInsumos = Action.async {
+    repo.list().map { res =>
+      Ok(Json.toJson(res))
+    }
+  }
+
+  def getRequestRowByInsumosByParent(id: Long) = Action.async {
+    repo.listByParent(id).map { res =>
+      Ok(Json.toJson(res))
+    }
+  }
+
+  // update required
+  val updateForm: Form[UpdateRequestRowByInsumoForm] = Form {
+    mapping(
+      "id" -> longNumber,
+      "requestId" -> longNumber,
+      "productId" -> longNumber,
+      "quantity" -> number,
+      "precio" -> of[Double],
+      "status" -> text
+    )(UpdateRequestRowByInsumoForm.apply)(UpdateRequestRowByInsumoForm.unapply)
+  }
+
+  // to copy
+  def show(id: Long) = Action {
+    Ok(views.html.requestRowByInsumo_show())
+  }
+
+  // update required
+  def getUpdate(id: Long) = Action.async {
+    repo.getById(id).map {case (res) =>
+      val anyData = Map("id" -> id.toString().toString(), "requestId" -> res.toList(0).requestId.toString(),
+                                "productId" -> res.toList(0).productId.toString(),
+                                "quantity" -> res.toList(0).quantity.toString(), "precio" -> res.toList(0).precio.toString(), "status" -> res.toList(0).status.toString())
+      val productReqNames = getProductReqNamesMap()
+      val insumoNames = getInsumoNamesMap()
+      Ok(views.html.requestRowByInsumo_update(updateForm.bind(anyData), productReqNames, insumoNames))
+    }
+  }
+
+  def getProductReqNamesMap(): Map[String, String] = {
+    Await.result(repoProductReq.getListNames().map{ case (res1) => 
+      val cache = collection.mutable.Map[String, String]()
+      res1.foreach{ case (key: Long, value: String) => 
+        cache put (key.toString(), value)
+      }
+      println(cache)
+      cache.toMap
+    }, 3000.millis)
+  }
+
+  def getInsumoNamesMap(): Map[String, String] = {
+    Await.result(repoInsum.getListNames().map{ case (res1) => 
+      val cache = collection.mutable.Map[String, String]()
+      res1.foreach{ case (key: Long, value: String) => 
+        cache put (key.toString(), value)
+      }
+      println(cache)
+      cache.toMap
+    }, 3000.millis)
+  }
+
+  def getProductorNamesMap(): Map[String, String] = {
+    Await.result(repoProductor.getListNames().map{ case (res1) => 
+      val cache = collection.mutable.Map[String, String]()
+      res1.foreach{ case (key: Long, value: String) => 
+        cache put (key.toString(), value)
+      }
+      println(cache)
+      cache.toMap
+    }, 3000.millis)
+  }
+
+
+  // update required
+  def getAccept(id: Long) = Action.async {
+    repo.acceptById(id).map {case (res) =>
+      repoInsum.updateAmount(res(0).productId, - res(0).quantity);
+      Redirect(routes.ProductRequestController.show(res(0).requestId))
+    }
+  }
+
+// update required
+  def getSend(id: Long) = Action.async {
+    repo.sendById(id).map {case (res) =>
+      Redirect(routes.ProductRequestController.show(res(0).requestId))
+    }
+  }
+
+// update required
+  def getFinish(id: Long) = Action.async {
+    repo.finishById(id).map {case (res) =>
+      Redirect(routes.ProductRequestController.show(res(0).requestId))
+    }
+  }
+
+  // delete required
+  def delete(id: Long) = Action.async {
+    repo.delete(id).map { res =>
+      Ok(views.html.requestRowByInsumo_index(Map[String, String](), Map[String, String]()))
+    }
+  }
+
+  // to copy
+  def getById(id: Long) = Action.async {
+    repo.getById(id).map { res =>
+      Ok(Json.toJson(res))
+    }
+  }
+
+  // update required
+  def updatePost = Action.async { implicit request =>
+    updateForm.bindFromRequest.fold(
+      errorForm => {
+        Future.successful(Ok(views.html.requestRowByInsumo_update(errorForm, Map[String, String](), Map[String, String]())))
+      },
+      res => {
+        repo.update(res.id, res.requestId, res.productId, res.quantity, res.precio, res.status).map { _ =>
+          Redirect(routes.ProductRequestController.show(res.requestId))
+        }
+      }
+    )
+  }
+
+}
+
+case class CreateRequestRowByInsumoForm(requestId: Long, productId: Long, quantity: Int, precio: Double, status: String)
+
+case class UpdateRequestRowByInsumoForm(id: Long, requestId: Long, productId: Long, quantity: Int, precio: Double, status: String)
